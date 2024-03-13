775344608c9b99b441456a1837398e095ddeec4f
Charles Pine <charlespine22@gmail.com>
2024-03-13 10:03:47 -0500

    Added home page assets

--- app/index.js
+++ app/index.js
@@ -1,10 +1,30 @@
-import { View, Text } from 'react-native';
+import { useState } from "react";
+import { View, ScrollView, SafeAreaView, Text } from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
 
 const Home = () => {
+  const router = useRouter();
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => {
+            <ScreenHeaderBtn />;
+          },
+        }}
+      />
+    </SafeAreaView>
   );
 };
 

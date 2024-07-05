ef80aae6358a20c39b89c005fd01a5a7728140c7
Charles Pine <charlespine22@gmail.com>
2024-07-05 15:27:55 -0500

    Added member list  to group details

--- components/common/groups/GroupDetails.jsx
+++ components/common/groups/GroupDetails.jsx
@@ -1,7 +1,7 @@
-import { View, Text, Pressable } from "react-native";
-import React from "react";
-import styles from "./groupDetails.style.js";
-import { FlatList } from "react-native-gesture-handler";
+import { View, Text, Pressable } from 'react-native';
+import React from 'react';
+import styles from './groupDetails.style.js';
+import { FlatList } from 'react-native-gesture-handler';
 
 const GroupDetails = ({ group, closeGroup }) => {
   console.log(group);
@@ -9,7 +9,16 @@ const GroupDetails = ({ group, closeGroup }) => {
     <View style={styles.groupDetailsWrapper}>
       <View style={styles.groupHeader}>
         <Text>{group.groupName}</Text>
-        <FlatList></FlatList>
+
+        <View style={styles.memberListContainer}>
+          <FlatList
+            horizontal={true}
+            // style={styles.memberList}
+            contentContainerStyle={styles.memberList}
+            data={group.members}
+            renderItem={({ item: member }) => <Text>{member}</Text>}
+          />
+        </View>
       </View>
       <Pressable onPress={closeGroup}>
         <Text>Close Group</Text>
--- components/common/groups/groupDetails.style.js
+++ components/common/groups/groupDetails.style.js
@@ -1,18 +1,30 @@
-import { StyleSheet } from "react-native";
-import { COLORS } from "../../../constants";
+import { StyleSheet } from 'react-native';
+import { COLORS, FONTS } from '../../../constants';
 
 const styles = StyleSheet.create({
   groupDetailsWrapper: {
     backgroundColor: COLORS.white,
-    position: "absolute",
-    height: "100%",
-    width: "100%",
+    position: 'absolute',
+    height: '100%',
+    width: '100%',
     zIndex: 1,
   },
   groupHeader: {
     flex: 1,
     // justifyContent: "center",
-    alignItems: "center",
+    alignItems: 'center',
+    width: '100%',
+  },
+  memberListContainer: {
+    width: '100%',
+    flexDirection: 'row',
+  },
+  memberList: {
+    display: 'flex',
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
   },
 });
 

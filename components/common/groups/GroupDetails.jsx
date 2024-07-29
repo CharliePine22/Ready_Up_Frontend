92aa5c8a4d1f4001c1dd08be1c6feddf7dc5781f
Charles Pine <charlespine22@gmail.com>
2024-07-29 18:53:59 -0500

    Adjusted spacing issues

--- components/common/groups/GroupDetails.jsx
+++ components/common/groups/GroupDetails.jsx
@@ -1,146 +1,128 @@
-import { View, Text, Pressable, FlatList, ScrollView } from 'react-native';
-import React from 'react';
-import styles from './groupDetails.style.js';
-import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import { View, Text, Pressable, FlatList, ScrollView } from "react-native";
+import React from "react";
+import styles from "./groupDetails.style.js";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 
 const GroupDetails = ({ group, closeGroup }) => {
   return (
     <View style={styles.groupDetailsWrapper}>
-      <View style={styles.groupHeader}>
-        <Text style={styles.groupName}>{group.groupName}</Text>
-        {/* Members List Wrapper */}
-        <View
-          style={{
-            borderBottomWidth: 2,
-            borderBottomStyle: 'solid',
-            borderColor: 'white',
-            marginBottom: 10,
-            width: 'auto',
-          }}
-        >
-          <Text
-            style={[
-              styles.headerFont,
-              {
-                color: 'white',
-                paddingHorizontal: 10,
-                lineHeight: 53,
-              },
-            ]}
-          >
-            Members
-          </Text>
+      <ScrollView>
+        <View style={styles.groupHeader}>
+          <Text style={styles.groupName}>{group.groupName}</Text>
+          {/* Members List Wrapper */}
+          <View style={styles.headerFontContainer}>
+            <Text
+              style={[
+                styles.headerFont,
+                {
+                  color: "white",
+                  paddingHorizontal: 10,
+                  lineHeight: 53,
+                },
+              ]}
+            >
+              Members
+            </Text>
+          </View>
+          <View style={styles.memberListContainer}>
+            <FlatList
+              horizontal={true}
+              contentContainerStyle={styles.memberList}
+              data={group.members}
+              renderItem={({ item: member }) => (
+                <Text
+                  style={[
+                    styles.baseFont,
+                    {
+                      // marginHorizontal: 20,
+                      marginVertical: 0,
+                      color: "white",
+                    },
+                  ]}
+                >
+                  {member}
+                </Text>
+              )}
+            />
+          </View>
         </View>
-        <View style={styles.memberListContainer}>
-          <FlatList
-            horizontal={true}
-            contentContainerStyle={styles.memberList}
-            data={group.members}
-            renderItem={({ item: member }) => (
+
+        <View style={styles.groupBodyWrapper}>
+          {/* GROUP GAME SELECTION */}
+          <View style={styles.gameSelectionWrapper}>
+            <View style={styles.headerFontContainer}>
               <Text
                 style={[
-                  styles.baseFont,
+                  styles.headerFont,
                   {
-                    // marginHorizontal: 20,
-                    marginVertical: 0,
-                    color: 'white',
+                    color: "white",
+                    paddingHorizontal: 10,
+                    lineHeight: 53,
                   },
                 ]}
               >
-                {member}
+                Game Selection
               </Text>
-            )}
-          />
+            </View>
+            <View style={styles.gameCaseContainer}>
+              <Text>?</Text>
+            </View>
+          </View>
+          {/* GROUP GAMES LIST */}
+          <View style={styles.headerFontContainer}>
+            <Text
+              style={[
+                styles.headerFont,
+                {
+                  paddingHorizontal: 10,
+                },
+              ]}
+            >
+              Games List
+            </Text>
+          </View>
+          <View style={styles.gamesPlayedListWrapper}>
+            <ScrollView showsVerticalScrollIndicator={false}>
+              {Object.entries(group.gamesPlayed)
+                .sort(([, a], [, b]) => b - a)
+                .map(([gameName, hoursPlayed], idx) => {
+                  return (
+                    <View key={gameName}>
+                      <Text
+                        style={[
+                          styles.gamesListGameName,
+                          {
+                            color:
+                              idx == 0
+                                ? "gold"
+                                : idx === 1
+                                ? "silver"
+                                : idx === 2
+                                ? "#905923"
+                                : "white",
+                            fontWeight: idx <= 2 && 900,
+                          },
+                        ]}
+                      >
+                        <>
+                          {gameName}: {hoursPlayed + " hours"}
+                        </>
+                      </Text>
+                    </View>
+                  );
+                })}
+            </ScrollView>
+          </View>
         </View>
-      </View>
 
-      {/* GROUP GAME SELECTION */}
-      <View style={styles.gameSelectionWrapper}>
-        <View
-          style={{
-            borderBottomWidth: 2,
-            borderBottomStyle: 'solid',
-            borderColor: 'white',
-            marginBottom: 10,
-            width: 'auto',
-          }}
-        >
-          <Text
-            style={[
-              styles.headerFont,
-              {
-                color: 'white',
-                paddingHorizontal: 10,
-                lineHeight: 53,
-              },
-            ]}
-          >
-            Game Selection
-          </Text>
-        </View>
-        <View style={styles.gameCaseContainer}>
-          <Text>?</Text>
-        </View>
-      </View>
-      {/* GROUP GAMES LIST */}
-      <View
-        style={{
-          borderBottomWidth: 2,
-          borderBottomStyle: 'solid',
-          borderColor: 'white',
-          marginBottom: 10,
-        }}
-      >
-        <Text
-          style={[
-            styles.headerFont,
-            {
-              paddingHorizontal: 10,
-            },
-          ]}
-        >
-          Games List
-        </Text>
-      </View>
-      <View style={styles.gamesPlayedListWrapper}>
-        <ScrollView showsVerticalScrollIndicator={false}>
-          {Object.entries(group.gamesPlayed)
-            .sort(([, a], [, b]) => b - a)
-            .map(([gameName, hoursPlayed], idx) => {
-              return (
-                <View key={gameName}>
-                  <Text
-                    style={[
-                      styles.gamesListGameName,
-                      {
-                        color:
-                          idx == 0
-                            ? 'gold'
-                            : idx === 1
-                            ? 'silver'
-                            : idx === 2
-                            ? '#905923'
-                            : 'white',
-                        fontWeight: idx <= 2 && 900,
-                      },
-                    ]}
-                  >
-                    <>
-                      {gameName}: {hoursPlayed + ' hours'}
-                    </>
-                  </Text>
-                </View>
-              );
-            })}
-        </ScrollView>
-      </View>
-      <Pressable onPress={closeGroup} style={styles.closeGroupBtn}>
-        <MaterialCommunityIcons
-          name='arrow-left-bold-outline'
-          size={40}
-          color={'white'}
-        />
-      </Pressable>
+        <Pressable onPress={closeGroup} style={styles.closeGroupBtn}>
+          <MaterialCommunityIcons
+            name="arrow-left-bold-outline"
+            size={40}
+            color={"white"}
+          />
+        </Pressable>
+      </ScrollView>
     </View>
   );
 };

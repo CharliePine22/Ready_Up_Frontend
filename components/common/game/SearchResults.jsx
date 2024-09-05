4fdcb1873fae019ca24bd604b5bf22355b713d97
Charles Pine <charlespine22@gmail.com>
2024-09-05 15:34:23 -0500

    Began working on styling for Search Results

--- components/common/game/SearchResults.jsx
+++ components/common/game/SearchResults.jsx
@@ -1,14 +1,23 @@
-import { View, Text, ActivityIndicator } from "react-native";
-import React from "react";
-import styles from "./searchResults.style";
+import { View, Text, ActivityIndicator, FlatList } from 'react-native';
+import React from 'react';
+import styles from './searchResults.style';
 
 const SearchResults = ({ game, searchResults, loading }) => {
   return (
     <View style={styles.searchResultsWrapper}>
       {loading ? (
-        <ActivityIndicator size="large" color="#FFF" />
+        <ActivityIndicator size='large' color='#FFF' />
       ) : (
-        <Text>{game}</Text>
+        <View style={styles.searchResultsContainer}>
+          <FlatList
+            data={searchResults}
+            renderItem={({ item: game }) => (
+              <View style={styles.searchResultItem}>
+                <Text>{game.name}</Text>
+              </View>
+            )}
+          />
+        </View>
       )}
     </View>
   );
--- components/common/game/searchResults.style.js
+++ components/common/game/searchResults.style.js
@@ -1,14 +1,18 @@
-import { StyleSheet } from "react-native";
-import { COLORS, FONT } from "../../../constants";
+import { StyleSheet } from 'react-native';
+import { COLORS, FONT } from '../../../constants';
 
 const styles = StyleSheet.create({
   searchResultsWrapper: {
-    width: "100%",
+    width: '100%',
     height: 250,
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  searchResultItem: {
+    display: 'flex',
+    flexDirection: 'row',
   },
 });
 

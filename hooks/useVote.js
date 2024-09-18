dfb36016dbde404dd3adcfe36f42a57029d02625
Charles Pine <charlespine22@gmail.com>
2024-09-17 19:45:40 -0500

    Added bare voting functionality

--- hooks/useVote.js
+++ hooks/useVote.js
@@ -1,8 +1,25 @@
-import { useState } from 'react';
+import { useState, useEffect } from "react";
 
 const useVote = () => {
   const [votedGames, setVotedGames] = useState([]);
-  const castVote = () => {};
+
+  const castVote = (game) => {
+    setVotedGames((votedGames) => [
+      ...votedGames,
+      {
+        name: game.name,
+        votes: (votes += 1),
+      },
+    ]);
+  };
+
+  const addNewGame = () => {};
+
+  const adjustTime = () => {};
+
+  const finalizeVote = () => {};
+
+  return { castVote, addNewGame, adjustTime, finalizeVote, votedGames };
 };
 
 export default useVote;

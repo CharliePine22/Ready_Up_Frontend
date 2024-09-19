4de96d89958545a7ed4c2c2a1e55fa1dcd5734c6
Charles Pine <charlespine22@gmail.com>
2024-09-19 18:42:50 -0500

    Began adding carousel

--- components/common/game/GameVoting.jsx
+++ components/common/game/GameVoting.jsx
@@ -1,21 +1,38 @@
-import { View, Text, ImageBackground, Pressable } from 'react-native';
-import React from 'react';
-import Carousel from 'react-native-snap-carousel';
-
+import { View, Text, ImageBackground, Pressable } from "react-native";
+import React from "react";
+import Carousel from "react-native-snap-carousel";
+import useVote from "../../../hooks/useVote";
 const GameVoting = () => {
+  const { votedGames } = useVote();
   return (
     <View style={styles.gameSelectionContainer}>
       <View
         style={[
           {
             padding: chosenGame ? 0 : 5,
-            borderStyle: chosenGame ? 'solid' : 'dotted',
+            borderStyle: chosenGame ? "solid" : "dotted",
           },
           styles.gameCaseContainer,
         ]}
       >
-        {!chosenGame ? (
-          <FontAwesome name='question' size={60} color={'white'} />
+        <Carousel
+          style={styles.test}
+          data={votedGames}
+          renderItem={({ item }) => (
+            <ImageBackground
+              style={styles.gameCaseCover}
+              source={{
+                uri: !chosenGame.cover.id
+                  ? chosenGame.cover
+                  : `https://images.igdb.com/igdb/image/upload/t_1080p/${chosenGame.cover.image_id}.jpg`,
+              }}
+            />
+          )}
+          sliderWidth={100}
+          itemWidth={100}
+        />
+        {/* {!chosenGame ? (
+          <FontAwesome name="question" size={60} color={"white"} />
         ) : (
           <ImageBackground
             style={styles.gameCaseCover}
@@ -25,20 +42,20 @@ const GameVoting = () => {
                 : `https://images.igdb.com/igdb/image/upload/t_1080p/${chosenGame.cover.image_id}.jpg`,
             }}
           />
-        )}
+        )} */}
       </View>
       <View style={styles.gameSelectionDetails}>
         <Text style={styles.gameMessage}>
           {!currentlyVoting
             ? "Your group hasn't chosen a game yet. As the leader of the group, suggest one to play!"
-            : 'Cast your vote, delay the session, or choose a superior game.'}
+            : "Cast your vote, delay the session, or choose a superior game."}
         </Text>
 
         <View style={styles.gameSelectionActions}>
           <Pressable
             style={({ pressed }) => [
               styles.gameSelectionBtn,
-              pressed ? styles.gameSelectionBtnPressed : '',
+              pressed ? styles.gameSelectionBtnPressed : "",
             ]}
             onPress={openGameSelection}
           >
@@ -47,7 +64,7 @@ const GameVoting = () => {
           <Pressable
             style={({ pressed }) => [
               styles.gameSelectionBtn,
-              pressed ? styles.gameSelectionBtnPressed : '',
+              pressed ? styles.gameSelectionBtnPressed : "",
             ]}
           >
             <Text style={styles.gameSelectionBtnText}>RANDOMIZE</Text>
